fede7e09d9ad45913b1ec8f7f3801f36a5f93698
shirok0408 <57366493+shirok0408@users.noreply.github.com>
2020-10-28 16:30:42 +0900

    2번 과제입니다~~ 예외상황에 대한 고려를 추가했습니다.

--- 2020192053 김희석 과제 2.py
+++ 2020192053 김희석 과제 2.py
@@ -2,18 +2,29 @@ import random
 import math
 import numpy
 
-A = [0.0, 0.0]
-B = [0.0, 0.0]
-C = [0.0, 0.0]
-d = 0.0
-t = 0.0
+A = [0.0, 0.0]*100
+B = [0.0, 0.0]*100
+C = [0.0, 0.0]*100
+d = [0.0]*100
+t = [0.0]*100
 
-for i in range(2):
+
+for i in range(101):
     A[i] = random.randrange(100)
     B[i] = random.randrange(100)
     C[i] = random.randrange(100)
 
-t = numpy.arccos((B[0]*C[0]-A[0]*C[0]+A[0]**2-B[0]*A[0]+B[1]*C[1]+A[1]**2-B[1]*A[1]-C[1]*A[1])/math.sqrt(((A[0]-B[0])**2+(A[1]-B[1])**2)*((A[0]-C[0])**2+(A[1]-C[1])**2)))/numpy.pi*180
-d = abs(B[0]*A[1]-B[1]*A[0]-C[0]*A[1]+A[0]*C[1]+B[1]*C[0]-B[0]*C[1])/(math.sqrt((B[0]-C[0])**2+(B[1]-C[1])**2))
+for i in range(0, 50):
+    if B[2*i]==C[2*i] and B[2*i+1]==C[2*i+1]:
+        d[i] = math.sqrt((A[2*i]-B[2*i])**2+(A[2*i+1]-B[2*i+1])**2)
+        t[i] = numpy.arccos((B[2*i]*C[2*i]-A[2*i]*C[2*i]+A[2*i]**2-B[2*i]*A[2*i]+B[2*i+1]*C[2*i+1]+A[2*i+1]**2-B[2*i+1]*A[2*i+1]-C[2*i+1]*A[2*i+1])/math.sqrt(((A[2*i]-B[2*i])**2+(A[2*i+1]-B[2*i+1])**2)*((A[2*i]-C[2*i])**2+(A[2*i+1]-C[2*i+1])**2)))/numpy.pi*180
+    if A[2*i]==C[2*i] and A[2*i+1]==C[2*i+1]:
+        t[i] = 0
+    if B[2*i]==A[2*i] and B[2*i+1]==A[2*i+1]:
+        t[i] = 0
+    else:
+        t[i] = numpy.arccos((B[2*i]*C[2*i]-A[2*i]*C[2*i]+A[2*i]**2-B[2*i]*A[2*i]+B[2*i+1]*C[2*i+1]+A[2*i+1]**2-B[2*i+1]*A[2*i+1]-C[2*i+1]*A[2*i+1])/math.sqrt(((A[2*i]-B[2*i])**2+(A[2*i+1]-B[2*i+1])**2)*((A[2*i]-C[2*i])**2+(A[2*i+1]-C[2*i+1])**2)))/numpy.pi*180
+        d[i] = abs(B[2*i]*A[2*i+1]-B[2*i+1]*A[2*i]-C[2*i]*A[2*i+1]+A[2*i]*C[2*i+1]+B[2*i+1]*C[2*i]-B[2*i]*C[2*i+1])/(math.sqrt((B[2*i]-C[2*i])**2+(B[2*i+1]-C[2*i+1])**2))
 
-print('A:(%d,%d), B:(%d,%d), C:(%d, %d) angle: %f(degree), distance: %f' % (A[0], A[1], B[0], B[1], C[0], C[1], t, d))
+for i in range(0, 50):
+    print('A:(%d,%d), B:(%d,%d), C:(%d, %d) angle: %f(degree), distance: %f' % (A[2*i], A[2*i+1], B[2*i], B[2*i+1], C[2*i], C[2*i+1], t[i], d[i]))
